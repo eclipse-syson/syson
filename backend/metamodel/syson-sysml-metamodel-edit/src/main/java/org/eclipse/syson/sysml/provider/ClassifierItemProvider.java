79b5c9d4e0e434055ad636febb856dbbb0040810
Mélanie Bats <melanie.bats@obeo.fr>
2024-01-17 17:00:31 +0100

    [enh] Contribute Classifier and DataType icons
    Signed-off-by: Mélanie Bats <melanie.bats@obeo.fr>
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

--- backend/metamodel/syson-sysml-metamodel-edit/src/main/java/org/eclipse/syson/sysml/provider/ClassifierItemProvider.java
+++ backend/metamodel/syson-sysml-metamodel-edit/src/main/java/org/eclipse/syson/sysml/provider/ClassifierItemProvider.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023 Obeo.
+ * Copyright (c) 2023, 2024 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -84,11 +84,11 @@ public class ClassifierItemProvider extends TypeItemProvider {
      * This returns Classifier.gif.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
+     * @generated NOT
      */
     @Override
     public Object getImage(Object object) {
-        return overlayImage(object, getResourceLocator().getImage("full/obj16/Classifier"));
+        return overlayImage(object, getResourceLocator().getImage("full/obj16/Classifier.svg"));
     }
 
     /**
--- backend/metamodel/syson-sysml-metamodel-edit/src/main/java/org/eclipse/syson/sysml/provider/DataTypeItemProvider.java
+++ backend/metamodel/syson-sysml-metamodel-edit/src/main/java/org/eclipse/syson/sysml/provider/DataTypeItemProvider.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023 Obeo.
+ * Copyright (c) 2023, 2024 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -59,11 +59,11 @@ public class DataTypeItemProvider extends ClassifierItemProvider {
      * This returns DataType.gif.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
+     * @generated NOT
      */
     @Override
     public Object getImage(Object object) {
-        return overlayImage(object, getResourceLocator().getImage("full/obj16/DataType"));
+        return overlayImage(object, getResourceLocator().getImage("full/obj16/DataType.svg"));
     }
 
     /**
