35a7d8c51ce4aa1c685aa5f37d56849094a000d5
Axel RICHARD <axel.richard@obeo.fr>
2024-03-08 19:38:32 +0100

    [97] Add a filter allowing to hide membership elements in the Explorer
    Bug: https://github.com/eclipse-syson/syson/issues/97
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

---
+++ backend/application/syson-application-configuration/src/main/java/org/eclipse/syson/application/configuration/HideMembershipsTreeItemAlteredContentProvider.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.syson.application.configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.sirius.components.representations.VariableManager;
+import org.eclipse.sirius.web.services.explorer.api.IExplorerTreeItemAlteredContentProvider;
+import org.eclipse.syson.sysml.Membership;
+import org.springframework.stereotype.Service;
+
+/**
+ * An implementation of {@link IExplorerTreeItemAlteredContentProvider} allowing to hide memberships tree items from
+ * Explorer tree without hide their children.
+ * 
+ * @author arichard
+ */
+@Service
+public class HideMembershipsTreeItemAlteredContentProvider implements IExplorerTreeItemAlteredContentProvider {
+
+    @Override
+    public boolean canHandle(Object object, List<String> activeFilterIds) {
+        return activeFilterIds.contains(SysONTreeFilterProvider.HIDE_MEMBERSHIPS_TREE_ITEM_FILTER_ID);
+    }
+
+    @Override
+    public List<Object> apply(List<Object> computedChildren, VariableManager variableManager) {
+        List<Object> alteredChildren = new ArrayList<>();
+        computedChildren.forEach(child -> {
+            if (child instanceof Membership membership) {
+                alteredChildren.addAll(membership.getOwnedRelatedElement());
+            } else {
+                alteredChildren.add(child);
+            }
+        });
+        return alteredChildren;
+    }
+
+}
