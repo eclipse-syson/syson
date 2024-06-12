7a6f4056f15700ba0bb8c51b609231e9da9ef532
Axel RICHARD <axel.richard@obeo.fr>
2024-06-12 15:40:58 +0200

    [releng] Fix update-sirius-web script
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

--- scripts/update-sirius-web.js
+++ scripts/update-sirius-web.js
@@ -40,6 +40,7 @@ projects.set("syson-diagram-common-view", "views");
 projects.set("syson-diagram-general-view", "views");
 projects.set("syson-diagram-interconnection-view", "views");
 projects.set("syson-diagram-statetransition-view", "views");
+projects.set("syson-diagram-tests", "views");
 
 console.log("Updating the following pom.xml:");
 projects.forEach((folder, project) => {
