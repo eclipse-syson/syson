0b020ee17d08b4a6f9da83aca6e75bfbd25a836e
Axel RICHARD <axel.richard@obeo.fr>
2024-06-18 10:35:04 +0200

    [416] Improve textual import running process
    Bug: https://github.com/eclipse-syson/syson/issues/416
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

--- backend/application/syson-sysml-import/src/main/java/org/eclipse/syson/sysml/SysmlToAst.java
+++ backend/application/syson-sysml-import/src/main/java/org/eclipse/syson/sysml/SysmlToAst.java
@@ -14,6 +14,7 @@ package org.eclipse.syson.sysml;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -25,7 +26,9 @@ import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.stereotype.Component;
 
 /**
@@ -36,35 +39,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class SysmlToAst {
 
-    private final String cliPath;
-
     private final Logger logger = LoggerFactory.getLogger(SysmlToAst.class);
-
-    public SysmlToAst(@Value("${org.eclipse.syson.syside.path}") String cliPath) {
-        this.cliPath = cliPath;
-    }
 
     public InputStream convert(InputStream input, String fileExtension) {
         InputStream output = null;
 
         try {
-            Path temp = Files.createTempFile("syson", "." + fileExtension);
-            OutputStream outStream = new FileOutputStream(temp.toString());
+            Path sysmlInputPath = this.createTempFile(input, "syson", fileExtension);
 
-            byte[] buffer = new byte[8 * 1024];
-            int bytesRead;
-            while ((bytesRead = input.read(buffer)) != -1) {
-                outStream.write(buffer, 0, bytesRead);
-            }
-            input.close();
-            outStream.close();
-            Path path = Path.of(this.cliPath);
-            this.logger.info("Call syside application : node " + path.toString() + " dump " + temp.toString());
-            String[] args = { "node", path.toString(), "dump", temp.toString() };
+            PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+            Resource resource = resolver.getResource(ResourcePatternResolver.CLASSPATH_URL_PREFIX + "syside-cli.js");
+            InputStream sysIdeInputStream = resource.getInputStream();
+
+            Path sysIdeInputPath = this.createTempFile(sysIdeInputStream, "syside-cli", "js");
+
+            this.logger.info("Call syside application : node " + sysIdeInputPath.toString() + " dump " + sysmlInputPath.toString());
+            String[] args = { "node", sysIdeInputPath.toString(), "dump", sysmlInputPath.toString() };
             ProcessBuilder pb = new ProcessBuilder(args);
             pb = pb.redirectErrorStream(false);
-            Process p = pb.start();
-            InputStream is = p.getInputStream();
+            Process sysIdeProcess = pb.start();
+            InputStream is = sysIdeProcess.getInputStream();
             InputStreamReader isr = new InputStreamReader(is);
             BufferedReader br = new BufferedReader(isr);
             StringBuilder builder = new StringBuilder();
@@ -79,8 +73,7 @@ public class SysmlToAst {
                     builder.append(line);
                 }
             } else {
-
-                InputStream er = p.getErrorStream();
+                InputStream er = sysIdeProcess.getErrorStream();
                 InputStreamReader err = new InputStreamReader(er);
                 BufferedReader erbr = new BufferedReader(err);
                 this.logger.error("Fail to call syside application : \n " + erbr.lines().collect(Collectors.joining("\n")));
@@ -88,14 +81,26 @@ public class SysmlToAst {
 
             output = new ByteArrayInputStream(builder.toString().getBytes());
 
-            temp.toFile().delete();
+            sysmlInputPath.toFile().delete();
+            sysIdeInputPath.toFile().delete();
 
         } catch (IOException e) {
             this.logger.error(e.getMessage());
         }
 
         return output;
-
     }
 
+    private Path createTempFile(InputStream input, String fileName, String fileExtension) throws IOException, FileNotFoundException {
+        Path inputPath = Files.createTempFile(fileName, "." + fileExtension);
+        OutputStream outStream = new FileOutputStream(inputPath.toString());
+        byte[] buffer = new byte[8 * 1024];
+        int bytesRead;
+        while ((bytesRead = input.read(buffer)) != -1) {
+            outStream.write(buffer, 0, bytesRead);
+        }
+        input.close();
+        outStream.close();
+        return inputPath;
+    }
 }
