76573597d0a318edd314f83a9d70b3e819550248
Axel RICHARD <axel.richard@obeo.fr>
2024-07-09 09:09:25 +0200

    [releng] Switch to Sirius Web 2024.5.5
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

--- scripts/update-sirius-web.js
+++ scripts/update-sirius-web.js
@@ -18,7 +18,7 @@ const newSiriusWebVersion = process.argv[2];
 
 if (!newSiriusWebVersion) {
   console.log("Use this script like this:");
-  console.log("node scripts/update-sirius-web.js 2024.5.4");
+  console.log("node scripts/update-sirius-web.js 2024.5.5");
   process.exit(1);
 }
 
