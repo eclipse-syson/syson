d083b23d852b25eb5c205a5e02335984658940f4
Axel RICHARD <axel.richard@obeo.fr>
2024-07-23 11:09:41 +0200

    [releng] Bump to 2024.7.1
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

--- scripts/update-sirius-web.js
+++ scripts/update-sirius-web.js
@@ -18,7 +18,7 @@ const newSiriusWebVersion = process.argv[2];
 
 if (!newSiriusWebVersion) {
   console.log("Use this script like this:");
-  console.log("node scripts/update-sirius-web.js 2024.7.0");
+  console.log("node scripts/update-sirius-web.js 2024.7.1");
   process.exit(1);
 }
 
