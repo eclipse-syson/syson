18c6e1e9423758bc2fc1058a7d5a8363b57ecdfd
Axel RICHARD <axel.richard@obeo.fr>
2025-02-03 10:59:13 +0100

    [releng] Bump version to 2025.1.5
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

--- integration-tests/cypress/e2e/project/diagrams/general-view/nodeCreationTests.cy.ts
+++ integration-tests/cypress/e2e/project/diagrams/general-view/nodeCreationTests.cy.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 Obeo.
+ * Copyright (c) 2024, 2025 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -43,7 +43,7 @@ describe('Node Creation Tests', () => {
 
     context('When we create an AttributeUsage using the diagram palette', () => {
       beforeEach(() => {
-        diagram.getDiagramBackground(diagramLabel).click();
+        diagram.getDiagramBackground(diagramLabel).rightclick().rightclick();
         diagram.getPalette().should('exist').findByTestId('toolSection-Structure').click();
         diagram.getPalette().should('exist').findByTestId('tool-New Attribute').click();
       });
@@ -76,7 +76,7 @@ describe('Node Creation Tests', () => {
 
     context('When we create an AttributeDefinition using the diagram palette', () => {
       beforeEach(() => {
-        diagram.getDiagramBackground(diagramLabel).click();
+        diagram.getDiagramBackground(diagramLabel).rightclick().rightclick();
         diagram.getPalette().should('exist').findByTestId('toolSection-Structure').click();
         diagram.getPalette().should('exist').findByTestId('tool-New Attribute Definition').click();
       });
