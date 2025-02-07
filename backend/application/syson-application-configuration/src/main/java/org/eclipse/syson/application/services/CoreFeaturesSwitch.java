0cfcf9619df4c3fff51f64f9a675a2aa757990c9
Arthur Daussy <arthur.daussy@obeo.fr>
2025-02-07 11:15:27 +0100

    [1006] Display isInitial and isDefault of FeatureValue Core tab
    Bug: https://github.com/eclipse-syson/syson/issues/1006
    Signed-off-by: Arthur Daussy <arthur.daussy@obeo.fr>

--- backend/application/syson-application-configuration/src/main/java/org/eclipse/syson/application/services/CoreFeaturesSwitch.java
+++ backend/application/syson-application-configuration/src/main/java/org/eclipse/syson/application/services/CoreFeaturesSwitch.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023, 2024 Obeo.
+ * Copyright (c) 2023, 2025 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -22,6 +22,7 @@ import org.eclipse.syson.sysml.Dependency;
 import org.eclipse.syson.sysml.Element;
 import org.eclipse.syson.sysml.Feature;
 import org.eclipse.syson.sysml.FeatureTyping;
+import org.eclipse.syson.sysml.FeatureValue;
 import org.eclipse.syson.sysml.LiteralBoolean;
 import org.eclipse.syson.sysml.LiteralInteger;
 import org.eclipse.syson.sysml.LiteralRational;
@@ -89,6 +90,15 @@ public class CoreFeaturesSwitch extends SysmlSwitch<List<EStructuralFeature>> {
         var features = new ArrayList<EStructuralFeature>();
         features.addAll(this.caseElement(object));
         features.add(SysmlPackage.eINSTANCE.getFeature_Direction());
+        return features;
+    }
+
+    @Override
+    public List<EStructuralFeature> caseFeatureValue(FeatureValue object) {
+        var features = new ArrayList<EStructuralFeature>();
+        features.addAll(this.caseMembership(object));
+        features.add(SysmlPackage.eINSTANCE.getFeatureValue_IsDefault());
+        features.add(SysmlPackage.eINSTANCE.getFeatureValue_IsInitial());
         return features;
     }
 
