9290e794241083a81a6ed00361ba080ba8168a41
Axel RICHARD <axel.richard@obeo.fr>
2025-02-25 17:45:29 +0100

    [releng] Bump version to 2025.1.8
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

--- frontend/syson/src/extensions/SysONFooter.tsx
+++ frontend/syson/src/extensions/SysONFooter.tsx
@@ -33,7 +33,7 @@ export const SysONFooter = ({}: FooterProps) => {
   return (
     <footer className={classes.footer}>
       <Typography variant="caption">
-        &copy; {new Date().getFullYear()} Obeo. SysON v2025.1.7. Powered by&nbsp;
+        &copy; {new Date().getFullYear()} Obeo. SysON v2025.1.8. Powered by&nbsp;
       </Typography>
       <Link variant="caption" href="https://www.eclipse.dev/sirius" rel="noopener noreferrer" target="_blank">
         Sirius Web
