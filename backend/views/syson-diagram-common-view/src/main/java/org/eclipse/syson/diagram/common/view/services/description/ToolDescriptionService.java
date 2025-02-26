3b4c27f16cc3f181969c421186e9155c94f6777f
Axel RICHARD <axel.richard@obeo.fr>
2025-02-26 11:37:25 +0100

    [fix] Fix diagram selection after tool execution
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

--- backend/views/syson-diagram-common-view/src/main/java/org/eclipse/syson/diagram/common/view/services/description/ToolDescriptionService.java
+++ backend/views/syson-diagram-common-view/src/main/java/org/eclipse/syson/diagram/common/view/services/description/ToolDescriptionService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 Obeo.
+ * Copyright (c) 2024, 2025 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -379,6 +379,7 @@ public class ToolDescriptionService {
                 .name(toolLabel)
                 .iconURLsExpression(iconPath.toString())
                 .body(changeContexMembership.build())
+                .elementsToSelectExpression("aql:newInstance")
                 .build();
     }
 
@@ -541,6 +542,7 @@ public class ToolDescriptionService {
                 .name(toolLabel)
                 .iconURLsExpression(iconPath.toString())
                 .body(createMembership.build())
+                .elementsToSelectExpression("aql:newInstance")
                 .build();
     }
 }
