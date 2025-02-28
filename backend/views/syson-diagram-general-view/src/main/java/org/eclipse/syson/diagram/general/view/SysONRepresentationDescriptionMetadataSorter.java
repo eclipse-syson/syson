fe48726cc4a18eec02f2de914be86d14e72fd1ef
Axel RICHARD <axel.richard@obeo.fr>
2025-02-28 10:05:20 +0100

    [releng] Switch to Sirius Web 2025.2.1
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

---
+++ backend/views/syson-diagram-general-view/src/main/java/org/eclipse/syson/diagram/general/view/SysONRepresentationDescriptionMetadataSorter.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.syson.diagram.general.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.sirius.components.collaborative.api.RepresentationDescriptionMetadata;
+import org.eclipse.sirius.components.emf.services.api.IRepresentationDescriptionMetadataSorter;
+import org.springframework.stereotype.Service;
+
+/**
+ * Sort representation descriptions in SysON. This sorter is used when creating a new representation in the frontend.
+ *
+ * @author arichard
+ */
+@Service
+public class SysONRepresentationDescriptionMetadataSorter implements IRepresentationDescriptionMetadataSorter {
+
+    @Override
+    public List<RepresentationDescriptionMetadata> sort(List<RepresentationDescriptionMetadata> representationDescriptions) {
+        List<RepresentationDescriptionMetadata> sortedRepresentationDescriptions = new ArrayList<>();
+        for (RepresentationDescriptionMetadata representationDescriptionMetadata : representationDescriptions) {
+            if (GeneralViewDiagramDescriptionProvider.DESCRIPTION_NAME.equals(representationDescriptionMetadata.getLabel())) {
+                sortedRepresentationDescriptions.add(0, representationDescriptionMetadata);
+            } else {
+                sortedRepresentationDescriptions.add(representationDescriptionMetadata);
+            }
+        }
+        return sortedRepresentationDescriptions;
+    }
+}
