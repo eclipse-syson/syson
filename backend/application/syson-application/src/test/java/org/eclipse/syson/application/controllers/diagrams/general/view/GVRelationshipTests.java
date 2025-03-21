47fe94e209d49e0ab0406371f8d3e34135ceb8fd
Arthur Daussy <arthur.daussy@obeo.fr>
2025-03-21 11:53:14 +0100

    [1120]  Implement 'ownedOccurrence' edge in General View
    Bug: https://github.com/eclipse-syson/syson/issues/1120
    Signed-off-by: Arthur Daussy <arthur.daussy@obeo.fr>

--- backend/application/syson-application/src/test/java/org/eclipse/syson/application/controllers/diagrams/general/view/GVRelationshipTests.java
+++ backend/application/syson-application/src/test/java/org/eclipse/syson/application/controllers/diagrams/general/view/GVRelationshipTests.java
@@ -22,9 +22,11 @@ import java.util.function.Consumer;
 
 import org.eclipse.sirius.components.collaborative.diagrams.dto.DiagramEventInput;
 import org.eclipse.sirius.components.collaborative.diagrams.dto.DiagramRefreshedEventPayload;
+import org.eclipse.sirius.components.diagrams.ArrowStyle;
+import org.eclipse.sirius.components.diagrams.Edge;
 import org.eclipse.sirius.web.tests.services.api.IGivenInitialServerState;
 import org.eclipse.syson.AbstractIntegrationTests;
-import org.eclipse.syson.application.data.SysMLv2Identifiers;
+import org.eclipse.syson.application.data.GeneraViewRelationshipIdentifiers;
 import org.eclipse.syson.services.diagrams.api.IGivenDiagramSubscription;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -45,36 +47,61 @@ import reactor.test.StepVerifier;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GVRelationshipTests extends AbstractIntegrationTests {
 
-
     @Autowired
     private IGivenInitialServerState givenInitialServerState;
 
     @Autowired
     private IGivenDiagramSubscription givenDiagramSubscription;
 
-
-    @Sql(scripts = {"/scripts/syson-test-database.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(scripts = {"/scripts/cleanup.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
+    @Sql(scripts = { "/scripts/syson-test-database.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = { "/scripts/cleanup.sql" }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
     @Test
     @DisplayName("Given a General View with multiplicity range, when the diagram is render, then the edge center label as an empty text")
     public void checkMultiplicityLabel() {
         this.givenInitialServerState.initialize();
         var diagramEventInput = new DiagramEventInput(UUID.randomUUID(),
-                SysMLv2Identifiers.GENERAL_VIEW_RELATIONSHIP_EDITING_CONTEXT_ID,
-                SysMLv2Identifiers.GENERAL_VIEW_RELATIONSHIP_DIAGRAM);
+                GeneraViewRelationshipIdentifiers.EDITING_CONTEXT_ID,
+                GeneraViewRelationshipIdentifiers.Diagram.ID);
         var flux = this.givenDiagramSubscription.subscribe(diagramEventInput);
 
         Consumer<DiagramRefreshedEventPayload> initialDiagramContentConsumer = payload -> Optional.of(payload)
                 .map(DiagramRefreshedEventPayload::diagram)
                 .ifPresentOrElse(diagram -> {
-                    assertThat(diagram.getNodes()).hasSize(2);
-                    assertThat(diagram.getEdges()).hasSize(1);
+                    assertThat(diagram.getNodes()).hasSize(4);
+                    assertThat(diagram.getEdges()).hasSize(2);
                     assertThat(diagram.getEdges().get(0).getCenterLabel()).isNotNull();
                     assertThat(diagram.getEdges().get(0).getCenterLabel().getText()).isBlank();
                     assertThat(diagram.getNodes()).satisfiesOnlyOnce(node -> assertThat(node.getInsideLabel().getText()).contains("[2]"));
 
                 }, () -> fail("Missing diagram"));
 
+        StepVerifier.create(flux)
+                .consumeNextWith(initialDiagramContentConsumer)
+                .thenCancel()
+                .verify(Duration.ofSeconds(10));
+    }
+
+    @Sql(scripts = { "/scripts/syson-test-database.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = { "/scripts/cleanup.sql" }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
+    @Test
+    @DisplayName("Given a General View with an OccurrenceDefinition owning a OccurrenceUsage, when the diagram is rendered, then an edge should be diplayed between the usage and the definition")
+    public void checkOccurenceDefinitionNestedOccurrencesEdge() {
+        this.givenInitialServerState.initialize();
+        var diagramEventInput = new DiagramEventInput(UUID.randomUUID(),
+                GeneraViewRelationshipIdentifiers.EDITING_CONTEXT_ID,
+                GeneraViewRelationshipIdentifiers.Diagram.ID);
+        var flux = this.givenDiagramSubscription.subscribe(diagramEventInput);
+
+        Consumer<DiagramRefreshedEventPayload> initialDiagramContentConsumer = payload -> Optional.of(payload)
+                .map(DiagramRefreshedEventPayload::diagram)
+                .ifPresentOrElse(diagram -> {
+                    Optional<Edge> nestedOcccurenceEdge = diagram.getEdges().stream().filter(
+                            edge -> GeneraViewRelationshipIdentifiers.Diagram.OCC_1.equals(edge.getTargetId()) && GeneraViewRelationshipIdentifiers.Diagram.OCC_DEF_1.equals(edge.getSourceId()))
+                            .findFirst();
+                    assertThat(nestedOcccurenceEdge.isPresent()).isTrue();
+                    assertThat(nestedOcccurenceEdge.get().getStyle().getSourceArrow() == ArrowStyle.FillDiamond);
+
+                }, () -> fail("Missing edge"));
 
         StepVerifier.create(flux)
                 .consumeNextWith(initialDiagramContentConsumer)
