ece3dd2ba95f6d2951ce7c80ee0770c489526e49
Arthur Daussy <arthur.daussy@obeo.fr>
2025-04-25 11:28:47 +0200

    [1252] Implement textual export of TriggerInvocationExpression
    Bug: https://github.com/eclipse-syson/syson/issues/1252
    Signed-off-by: Arthur Daussy <arthur.daussy@obeo.fr>

--- backend/application/syson-application/src/test/java/org/eclipse/syson/application/export/ImportExportTests.java
+++ backend/application/syson-application/src/test/java/org/eclipse/syson/application/export/ImportExportTests.java
@@ -221,16 +221,25 @@ public class ImportExportTests extends AbstractIntegrationTests {
     @DisplayName("Given a model with AcceptActionUsage, when importing/exporting the file, then the exported text file should be the same as the imported one.")
     public void checkAcceptActionUsage() throws IOException {
         var input = """
-                action a1 {
-                    item def S1;
-                    item def S2;
-                    item def S3;
-                    port p1;
-                    port p2;
-                    port p3;
-                    action a1 accept s1 : S1 via p1;
-                    action a2 accept S2 via p2;
-                    accept S3 via p3;
+                part part1 {
+                    private import SI::*;
+                    private import ScalarValues::*;
+                    action b {
+                        attribute f : Boolean;
+                    }
+                    action a1 {
+                        item def S1;
+                        item def S2;
+                        item def S3;
+                        port p1;
+                        port p2;
+                        port p3;
+                        action a1 accept s1 : S1 via p1;
+                        action a2 accept S2 via p2;
+                        accept S3 via p3;
+                        accept after 5 [minute];
+                        accept when b.f;
+                    }
                 }""";
         this.checker.check(input, input);
     }
