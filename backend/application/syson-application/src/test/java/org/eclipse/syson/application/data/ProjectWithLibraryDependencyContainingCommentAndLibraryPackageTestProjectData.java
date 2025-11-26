6ace297692f2d8b8ccb0cd2a4353176e5c126886
Gwendal Daniel <gwendal.daniel@obeosoft.com>
2025-11-26 14:47:05 +0100

    [1698] Discard non-Namespace elements to check user libraries
    Bug: https://github.com/eclipse-syson/syson/issues/1698
    Signed-off-by: Gwendal Daniel <gwendal.daniel@obeosoft.com>

---
+++ backend/application/syson-application/src/test/java/org/eclipse/syson/application/data/ProjectWithLibraryDependencyContainingCommentAndLibraryPackageTestProjectData.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.syson.application.data;
+
+/**
+ * Identifiers for the "ProjectWithLibraryDependencyContainingPackageAndLibraryPackage" project.
+ *
+ * @author gdaniel
+ */
+public class ProjectWithLibraryDependencyContainingCommentAndLibraryPackageTestProjectData {
+
+    public static final String SCRIPT_PATH = "/scripts/database-content/SysMLv2-ProjectWithLibraryDependencyContainingCommentAndLibraryPackage.sql";
+
+    public static final String PROJECT_NAME = "SysMLv2";
+
+    public static final String PROJECT_ID = "be5101e5-27ac-41bc-b050-cb9c87ed4bc7";
+
+    public static final String EDITING_CONTEXT = "00fdbaac-73bc-4188-8758-0070e47b993c";
+
+    public static final String LIBRARY_EDITING_CONTEXT = "732e2e1a-04b5-4539-a60a-3fee14c033d8";
+}
