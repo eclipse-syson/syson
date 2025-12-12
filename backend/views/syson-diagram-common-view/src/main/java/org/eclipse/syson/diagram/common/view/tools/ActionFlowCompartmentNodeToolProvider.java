dd4036427ed09cb76447bd2efd9385a1e23899e0
Pierre-Charles David <pierre-charles.david@obeo.fr>
2025-12-12 09:44:14 +0100

    [1747] Automatically select more newly created elements
    Bug: https://github.com/eclipse-syson/syson/issues/1747
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- backend/views/syson-diagram-common-view/src/main/java/org/eclipse/syson/diagram/common/view/tools/ActionFlowCompartmentNodeToolProvider.java
+++ backend/views/syson-diagram-common-view/src/main/java/org/eclipse/syson/diagram/common/view/tools/ActionFlowCompartmentNodeToolProvider.java
@@ -12,8 +12,6 @@
  *******************************************************************************/
 package org.eclipse.syson.diagram.common.view.tools;
 
-import java.util.List;
-
 import org.eclipse.sirius.components.collaborative.diagrams.DiagramContext;
 import org.eclipse.sirius.components.core.api.IEditingContext;
 import org.eclipse.sirius.components.diagrams.Node;
@@ -23,8 +21,10 @@ import org.eclipse.sirius.components.view.builder.generated.view.ViewBuilders;
 import org.eclipse.sirius.components.view.builder.providers.INodeToolProvider;
 import org.eclipse.sirius.components.view.diagram.NodeTool;
 import org.eclipse.sirius.components.view.emf.diagram.ViewDiagramDescriptionConverter;
+import org.eclipse.syson.diagram.common.view.services.ViewCreateService;
+import org.eclipse.syson.diagram.common.view.services.ViewNodeService;
 import org.eclipse.syson.diagram.services.aql.DiagramMutationAQLService;
-import org.eclipse.syson.util.AQLUtils;
+import org.eclipse.syson.util.AQLConstants;
 import org.eclipse.syson.util.ServiceMethod;
 
 /**
@@ -33,6 +33,8 @@ import org.eclipse.syson.util.ServiceMethod;
  * @author Jerome Gout
  */
 public class ActionFlowCompartmentNodeToolProvider implements INodeToolProvider {
+
+    private static final String NEW_INSTANCE = "newInstance";
 
     private final DiagramBuilders diagramBuilderHelper = new DiagramBuilders();
 
@@ -47,18 +49,19 @@ public class ActionFlowCompartmentNodeToolProvider implements INodeToolProvider 
                         ViewDiagramDescriptionConverter.CONVERTED_NODES_VARIABLE));
 
         var revealOperation = this.viewBuilderHelper.newChangeContext()
-                .expression(
-                        AQLUtils.getServiceCallExpression(Node.SELECTED_NODE, "revealCompartment",
-                                List.of("self", DiagramContext.DIAGRAM_CONTEXT, IEditingContext.EDITING_CONTEXT, ViewDiagramDescriptionConverter.CONVERTED_NODES_VARIABLE)));
+                .expression(ServiceMethod.of4(ViewNodeService::revealCompartment).aql(Node.SELECTED_NODE, AQLConstants.SELF, DiagramContext.DIAGRAM_CONTEXT, IEditingContext.EDITING_CONTEXT,
+                        ViewDiagramDescriptionConverter.CONVERTED_NODES_VARIABLE));
 
-        var creationServiceCall = this.viewBuilderHelper.newChangeContext()
-                .expression(AQLUtils.getSelfServiceCallExpression("createSubActionUsage"))
+        var letNewInstance = this.viewBuilderHelper.newLet()
+                .variableName(NEW_INSTANCE)
+                .valueExpression(ServiceMethod.of0(ViewCreateService::createSubActionUsage).aqlSelf())
                 .children(addToExposedElements.build(), revealOperation.build())
                 .build();
 
         return builder.name("New Action")
                 .iconURLsExpression("/icons/full/obj16/ActionUsage.svg")
-                .body(creationServiceCall)
+                .body(letNewInstance)
+                .elementsToSelectExpression(AQLConstants.AQL + NEW_INSTANCE)
                 .build();
     }
 }
