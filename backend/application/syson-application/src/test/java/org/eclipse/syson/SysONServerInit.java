e866cd4a08d610b698c0bf02b7f2d01f56dd6266
Théo Giraudet <theo.giraudet@obeo.fr>
2026-01-27 10:25:14 +0100

    [test] Add new GivenSysONServer annotation
    Signed-off-by: Théo Giraudet <theo.giraudet@obeo.fr>

---
+++ backend/application/syson-application/src/test/java/org/eclipse/syson/SysONServerInit.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.syson;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+
+/**
+ * Executes SQL scripts before the test method in an isolated transaction.
+ *
+ * @author theogiraudet
+ */
+@Documented
+@Target({ ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
+public @interface SysONServerInit {
+
+    /**
+     * SQL scripts to execute before the test.
+     */
+    @AliasFor(annotation = Sql.class, attribute = "scripts")
+    String[] value() default {};
+
+}
