31cf12c890648ef988151cbe08f4decc7ba3cd16
Théo Giraudet <theo.giraudet@obeo.fr>
2026-01-28 11:58:49 +0100

    [cleanup] Inline StepVerifier logic in GVAnnotatingElementTests
    Signed-off-by: Théo Giraudet <theo.giraudet@obeo.fr>

--- backend/application/syson-application/src/test/java/org/eclipse/syson/application/controllers/diagrams/general/view/GVAnnotatingElementTests.java
+++ backend/application/syson-application/src/test/java/org/eclipse/syson/application/controllers/diagrams/general/view/GVAnnotatingElementTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 Obeo.
+ * Copyright (c) 2025, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -13,39 +13,42 @@
 package org.eclipse.syson.application.controllers.diagrams.general.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.eclipse.sirius.components.diagrams.tests.DiagramEventPayloadConsumer.assertRefreshedDiagramThat;
+
+import com.jayway.jsonpath.JsonPath;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
 
 import org.eclipse.sirius.components.collaborative.diagrams.dto.DiagramEventInput;
 import org.eclipse.sirius.components.collaborative.diagrams.dto.DiagramRefreshedEventPayload;
+import org.eclipse.sirius.components.collaborative.diagrams.dto.EditLabelInput;
+import org.eclipse.sirius.components.collaborative.diagrams.dto.EditLabelSuccessPayload;
 import org.eclipse.sirius.components.core.api.IObjectSearchService;
 import org.eclipse.sirius.components.diagrams.Diagram;
 import org.eclipse.sirius.components.diagrams.Edge;
+import org.eclipse.sirius.components.diagrams.InsideLabel;
 import org.eclipse.sirius.components.diagrams.Node;
 import org.eclipse.sirius.components.diagrams.tests.graphql.EditLabelMutationRunner;
 import org.eclipse.sirius.components.diagrams.tests.graphql.InitialDirectEditElementLabelQueryRunner;
-import org.eclipse.sirius.components.view.diagram.DiagramDescription;
+import org.eclipse.sirius.components.diagrams.tests.navigation.DiagramNavigator;
+import org.eclipse.sirius.components.graphql.tests.ExecuteEditingContextFunctionSuccessPayload;
 import org.eclipse.sirius.components.view.emf.diagram.IDiagramIdProvider;
 import org.eclipse.sirius.web.tests.services.api.IGivenInitialServerState;
 import org.eclipse.syson.AbstractIntegrationTests;
+import org.eclipse.syson.GivenSysONServer;
 import org.eclipse.syson.application.controller.editingContext.checkers.ISemanticChecker;
-import org.eclipse.syson.application.controller.editingContext.checkers.SemanticCheckerService;
 import org.eclipse.syson.application.controllers.diagrams.checkers.CheckDiagramElementCount;
-import org.eclipse.syson.application.controllers.diagrams.checkers.DiagramCheckerService;
-import org.eclipse.syson.application.controllers.diagrams.checkers.IDiagramChecker;
-import org.eclipse.syson.application.controllers.diagrams.testers.DirectEditInitialLabelTester;
-import org.eclipse.syson.application.controllers.diagrams.testers.DirectEditTester;
 import org.eclipse.syson.application.controllers.diagrams.testers.ToolTester;
 import org.eclipse.syson.application.data.GeneralViewWithTopNodesTestProjectData;
 import org.eclipse.syson.services.SemanticRunnableFactory;
 import org.eclipse.syson.services.diagrams.DiagramComparator;
 import org.eclipse.syson.services.diagrams.DiagramDescriptionIdProvider;
-import org.eclipse.syson.services.diagrams.NodeCreationTestsService;
 import org.eclipse.syson.services.diagrams.api.IGivenDiagramDescription;
-import org.eclipse.syson.services.diagrams.api.IGivenDiagramReference;
 import org.eclipse.syson.services.diagrams.api.IGivenDiagramSubscription;
 import org.eclipse.syson.standard.diagrams.view.SDVDescriptionNameGenerator;
 import org.eclipse.syson.sysml.Element;
@@ -55,18 +58,16 @@ import org.eclipse.syson.sysml.TextualRepresentation;
 import org.eclipse.syson.sysml.helper.EMFUtils;
 import org.eclipse.syson.util.IDescriptionNameGenerator;
 import org.eclipse.syson.util.SysONRepresentationDescriptionIdentifiers;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.transaction.annotation.Transactional;
 
+import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
-import reactor.test.StepVerifier.Step;
 
 /**
  * Tests several actions on AnnotatingElement of the General View diagram.
@@ -79,9 +80,6 @@ public class GVAnnotatingElementTests extends AbstractIntegrationTests {
 
     @Autowired
     private IGivenInitialServerState givenInitialServerState;
-
-    @Autowired
-    private IGivenDiagramReference givenDiagram;
 
     @Autowired
     private IGivenDiagramDescription givenDiagramDescription;
@@ -110,65 +108,47 @@ public class GVAnnotatingElementTests extends AbstractIntegrationTests {
     @Autowired
     private InitialDirectEditElementLabelQueryRunner initialDirectEditElementLabelQueryRunner;
 
-    private DiagramDescriptionIdProvider diagramDescriptionIdProvider;
-
-    private Step<DiagramRefreshedEventPayload> verifier;
-
-    private AtomicReference<Diagram> diagram;
-
-    private DiagramDescription diagramDescription;
-
-    private DiagramCheckerService diagramCheckerService;
-
-    private SemanticCheckerService semanticCheckerService;
-
-    private DirectEditInitialLabelTester directEditInitialLabelTester;
-
-    private NodeCreationTestsService creationTestsService;
-
     private final IDescriptionNameGenerator descriptionNameGenerator = new SDVDescriptionNameGenerator();
 
-    private DirectEditTester directEditTester;
+    private Flux<DiagramRefreshedEventPayload> givenSubscriptionToDiagram() {
+        var diagramEventInput = new DiagramEventInput(UUID.randomUUID(), GeneralViewWithTopNodesTestProjectData.EDITING_CONTEXT_ID, GeneralViewWithTopNodesTestProjectData.GraphicalIds.DIAGRAM_ID);
+        return this.givenDiagramSubscription.subscribe(diagramEventInput);
+    }
 
     @BeforeEach
     public void setUp() {
         this.givenInitialServerState.initialize();
-        var diagramEventInput = new DiagramEventInput(UUID.randomUUID(),
-                GeneralViewWithTopNodesTestProjectData.EDITING_CONTEXT_ID,
-                GeneralViewWithTopNodesTestProjectData.GraphicalIds.DIAGRAM_ID);
-        var flux = this.givenDiagramSubscription.subscribe(diagramEventInput);
-        this.verifier = StepVerifier.create(flux);
-        this.diagram = this.givenDiagram.getDiagram(this.verifier);
-        this.diagramDescription = this.givenDiagramDescription.getDiagramDescription(GeneralViewWithTopNodesTestProjectData.EDITING_CONTEXT_ID,
-                SysONRepresentationDescriptionIdentifiers.GENERAL_VIEW_DIAGRAM_DESCRIPTION_ID);
-        this.diagramDescriptionIdProvider = new DiagramDescriptionIdProvider(this.diagramDescription, this.diagramIdProvider);
-        this.diagramCheckerService = new DiagramCheckerService(this.diagramComparator, this.descriptionNameGenerator);
-        this.semanticCheckerService = new SemanticCheckerService(this.semanticRunnableFactory, this.objectSearchService, GeneralViewWithTopNodesTestProjectData.EDITING_CONTEXT_ID,
-                GeneralViewWithTopNodesTestProjectData.SemanticIds.PACKAGE_1_ID);
-        this.creationTestsService = new NodeCreationTestsService(this.nodeCreationTester, this.descriptionNameGenerator, GeneralViewWithTopNodesTestProjectData.EDITING_CONTEXT_ID);
-        this.directEditInitialLabelTester = new DirectEditInitialLabelTester(this.initialDirectEditElementLabelQueryRunner, GeneralViewWithTopNodesTestProjectData.EDITING_CONTEXT_ID);
-        this.directEditTester = new DirectEditTester(this.editLabelMutationRunner, GeneralViewWithTopNodesTestProjectData.EDITING_CONTEXT_ID);
-    }
-
-    @AfterEach
-    public void tearDown() {
-        if (this.verifier != null) {
-            this.verifier.thenCancel()
-                    .verify(Duration.ofSeconds(10));
-        }
     }
 
     @DisplayName("GIVEN a Part Definition, WHEN using the 'New TextualRepresentation' tool, THEN a new node should be created with an edge connecting it to the PartDefinition")
     @Test
-    @Sql(scripts = { GeneralViewWithTopNodesTestProjectData.SCRIPT_PATH }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
-            config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
-    @Sql(scripts = { "/scripts/cleanup.sql" }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
+    @GivenSysONServer({ GeneralViewWithTopNodesTestProjectData.SCRIPT_PATH })
     public void createTextualRepresentation() {
-        String parentLabel = "part";
-        this.creationTestsService.createNode(this.verifier, this.diagramDescriptionIdProvider, this.diagram, SysmlPackage.eINSTANCE.getPartUsage(), parentLabel,
-                this.descriptionNameGenerator.getCreationToolName(SysmlPackage.eINSTANCE.getTextualRepresentation()));
+        var flux = this.givenSubscriptionToDiagram();
 
-        IDiagramChecker diagramChecker = (initialDiagram, newDiagram) -> {
+        var diagramDescription = this.givenDiagramDescription.getDiagramDescription(GeneralViewWithTopNodesTestProjectData.EDITING_CONTEXT_ID,
+                SysONRepresentationDescriptionIdentifiers.GENERAL_VIEW_DIAGRAM_DESCRIPTION_ID);
+        var diagramDescriptionIdProvider = new DiagramDescriptionIdProvider(diagramDescription, this.diagramIdProvider);
+
+        AtomicReference<Diagram> diagram = new AtomicReference<>();
+        Consumer<Object> initialDiagramContentConsumer = assertRefreshedDiagramThat(diagram::set);
+
+        String parentLabel = "part";
+
+        String creationToolId = diagramDescriptionIdProvider.getNodeToolId(
+                this.descriptionNameGenerator.getNodeName(SysmlPackage.eINSTANCE.getPartUsage()),
+                this.descriptionNameGenerator.getCreationToolName(SysmlPackage.eINSTANCE.getTextualRepresentation())
+        );
+
+        Runnable invokeCreationToolRunnable = () -> this.nodeCreationTester.invokeTool(GeneralViewWithTopNodesTestProjectData.EDITING_CONTEXT_ID,
+                diagram,
+                parentLabel,
+                creationToolId,
+                List.of()
+        );
+
+        Consumer<Object> diagramCheckerConsumer = assertRefreshedDiagramThat(newDiagram -> {
+            var initialDiagram = diagram.get();
             new CheckDiagramElementCount(this.diagramComparator)
                     .hasNewNodeCount(1)
                     .hasNewEdgeCount(1)
@@ -180,9 +160,7 @@ public class GVAnnotatingElementTests extends AbstractIntegrationTests {
             Edge newEdge = this.diagramComparator.newEdges(initialDiagram, newDiagram).get(0);
             assertThat(newEdge.getSourceId()).isEqualTo(newNode.getId());
             assertThat(newEdge.getTargetId()).isEqualTo(GeneralViewWithTopNodesTestProjectData.GraphicalIds.PART_USAGE_ID);
-        };
-
-        this.diagramCheckerService.checkDiagram(diagramChecker, this.diagram, this.verifier);
+        });
 
         ISemanticChecker semanticChecker = (editingContext) -> {
             Object semanticRootObject = this.objectSearchService.getObject(editingContext, GeneralViewWithTopNodesTestProjectData.SemanticIds.PART_USAGE_ID).orElse(null);
@@ -193,30 +171,82 @@ public class GVAnnotatingElementTests extends AbstractIntegrationTests {
             assertThat(textualRepresentations).hasSize(1).allMatch(t -> t.getBody().equals("add textual representation here"));
         };
 
-        this.semanticCheckerService.checkEditingContext(semanticChecker, this.verifier);
+        Runnable editingContextCheckerRunnable = this.semanticRunnableFactory.createRunnable(GeneralViewWithTopNodesTestProjectData.EDITING_CONTEXT_ID,
+                (editingContext, executeEditingContextFunctionInput) -> {
+                    semanticChecker.check(editingContext);
+                    return new ExecuteEditingContextFunctionSuccessPayload(executeEditingContextFunctionInput.id(), true);
+                });
+
+        StepVerifier.create(flux)
+                .consumeNextWith(initialDiagramContentConsumer)
+                .then(invokeCreationToolRunnable)
+                .consumeNextWith(diagramCheckerConsumer)
+                .then(editingContextCheckerRunnable)
+                .thenCancel()
+                .verify(Duration.ofSeconds(10));
     }
 
     @DisplayName("GIVEN a TextualRepresentation, WHEN using the 'Direct Edit' tool, THEN the body of the textual representation should be updated")
     @Test
-    @Sql(scripts = { GeneralViewWithTopNodesTestProjectData.SCRIPT_PATH }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
-            config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
-    @Sql(scripts = { "/scripts/cleanup.sql" }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
+    @GivenSysONServer({ GeneralViewWithTopNodesTestProjectData.SCRIPT_PATH })
     public void directEditTextualRepresentation() {
-        this.directEditInitialLabelTester.checkDirectEditInitialLabelOnNode(this.verifier, this.diagram, GeneralViewWithTopNodesTestProjectData.GraphicalIds.PART_DEFINITION_TEXTUAL_REP_ID,
-                "add textual representation here");
+        var flux = this.givenSubscriptionToDiagram();
 
-        this.directEditTester.checkDirectEditInsideLabel(this.verifier, this.diagram, GeneralViewWithTopNodesTestProjectData.GraphicalIds.PART_DEFINITION_TEXTUAL_REP_ID, "Some textual representation",
-                "«rep»\n"
-                + "\n"
-                + "Some textual representation");
+        AtomicReference<Diagram> diagram = new AtomicReference<>();
+        Consumer<Object> initialDiagramContentConsumer = assertRefreshedDiagramThat(diagram::set);
 
-        ISemanticChecker semanticChecker = (editingContext) -> {
-            Object element = this.objectSearchService.getObject(editingContext, GeneralViewWithTopNodesTestProjectData.SemanticIds.PART_DEFINITION_TEXTUAL_REP_ID).orElse(null);
-            assertThat(element).isInstanceOf(TextualRepresentation.class);
-            TextualRepresentation textualRepresentation = (TextualRepresentation) element;
-            assertThat(textualRepresentation.getBody()).isEqualTo("Some textual representation");
+        Runnable checkDirectEditInitialLabelOnNodeRunnable = () -> {
+            DiagramNavigator diagramNavigator = new DiagramNavigator(diagram.get());
+            String labelId = diagramNavigator.nodeWithId(GeneralViewWithTopNodesTestProjectData.GraphicalIds.PART_DEFINITION_TEXTUAL_REP_ID).getNode().getInsideLabel().getId();
+
+            Map<String, Object> variables = Map.of(
+                    "editingContextId", GeneralViewWithTopNodesTestProjectData.EDITING_CONTEXT_ID,
+                    "diagramId", diagram.get().getId(),
+                    "labelId", labelId);
+            var initialDirectEditElementLabelResult = this.initialDirectEditElementLabelQueryRunner.run(variables);
+
+            String initialDirectEditElementLabel = JsonPath.read(initialDirectEditElementLabelResult.data(), "$.data.viewer.editingContext.representation.description.initialDirectEditElementLabel");
+            assertThat(initialDirectEditElementLabel).isEqualTo("add textual representation here");
         };
 
-        this.semanticCheckerService.checkEditingContext(semanticChecker, this.verifier);
+        Runnable requestDirectEdit = () -> {
+            DiagramNavigator diagramNavigator = new DiagramNavigator(diagram.get());
+            String labelId = diagramNavigator.nodeWithId(GeneralViewWithTopNodesTestProjectData.GraphicalIds.PART_DEFINITION_TEXTUAL_REP_ID).getNode().getInsideLabel().getId();
+
+            EditLabelInput input = new EditLabelInput(UUID.randomUUID(), GeneralViewWithTopNodesTestProjectData.EDITING_CONTEXT_ID, diagram.get().getId(), labelId, "Some textual representation");
+            var result = this.editLabelMutationRunner.run(input);
+
+            String typename = JsonPath.read(result.data(), "$.data.editLabel.__typename");
+            assertThat(typename).isEqualTo(EditLabelSuccessPayload.class.getSimpleName());
+        };
+
+        Consumer<Object> checker = assertRefreshedDiagramThat(newDiagram -> {
+            DiagramNavigator diagramNavigator = new DiagramNavigator(newDiagram);
+
+            InsideLabel newLabel = diagramNavigator.nodeWithId(GeneralViewWithTopNodesTestProjectData.GraphicalIds.PART_DEFINITION_TEXTUAL_REP_ID).getNode().getInsideLabel();
+            assertThat(newLabel.getText()).isEqualTo("""
+                    «rep»
+                    
+                    Some textual representation""");
+        });
+
+        Runnable runnableChecker = this.semanticRunnableFactory.createRunnable(GeneralViewWithTopNodesTestProjectData.EDITING_CONTEXT_ID,
+                (editingContext, executeEditingContextFunctionInput) -> {
+                    Object element = this.objectSearchService.getObject(editingContext, GeneralViewWithTopNodesTestProjectData.SemanticIds.PART_DEFINITION_TEXTUAL_REP_ID).orElse(null);
+                    assertThat(element).isInstanceOf(TextualRepresentation.class);
+                    TextualRepresentation textualRepresentation = (TextualRepresentation) element;
+                    Assertions.assertNotNull(textualRepresentation);
+                    assertThat(textualRepresentation.getBody()).isEqualTo("Some textual representation");
+                    return new ExecuteEditingContextFunctionSuccessPayload(executeEditingContextFunctionInput.id(), true);
+                });
+
+        StepVerifier.create(flux)
+                .consumeNextWith(initialDiagramContentConsumer)
+                .then(checkDirectEditInitialLabelOnNodeRunnable)
+                .then(requestDirectEdit)
+                .consumeNextWith(checker)
+                .then(runnableChecker)
+                .thenCancel()
+                .verify(Duration.ofSeconds(10));
     }
 }
