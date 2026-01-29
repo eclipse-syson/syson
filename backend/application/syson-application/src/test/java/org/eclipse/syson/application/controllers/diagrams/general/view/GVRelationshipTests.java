26cacbacc994195606c4ac3b39dd2cc327e9937a
Théo Giraudet <theo.giraudet@obeo.fr>
2026-01-29 16:22:07 +0100

    [cleanup] Update tests GVRelationshipTests
    Signed-off-by: Théo Giraudet <theo.giraudet@obeo.fr>

--- backend/application/syson-application/src/test/java/org/eclipse/syson/application/controllers/diagrams/general/view/GVRelationshipTests.java
+++ backend/application/syson-application/src/test/java/org/eclipse/syson/application/controllers/diagrams/general/view/GVRelationshipTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 Obeo.
+ * Copyright (c) 2025, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -13,7 +13,7 @@
 package org.eclipse.syson.application.controllers.diagrams.general.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.eclipse.sirius.components.diagrams.tests.DiagramEventPayloadConsumer.assertRefreshedDiagramThat;
 
 import java.time.Duration;
 import java.util.Optional;
@@ -21,11 +21,11 @@ import java.util.UUID;
 import java.util.function.Consumer;
 
 import org.eclipse.sirius.components.collaborative.diagrams.dto.DiagramEventInput;
-import org.eclipse.sirius.components.collaborative.diagrams.dto.DiagramRefreshedEventPayload;
 import org.eclipse.sirius.components.diagrams.ArrowStyle;
 import org.eclipse.sirius.components.diagrams.Edge;
 import org.eclipse.sirius.web.tests.services.api.IGivenInitialServerState;
 import org.eclipse.syson.AbstractIntegrationTests;
+import org.eclipse.syson.GivenSysONServer;
 import org.eclipse.syson.application.data.GeneraViewRelationshipTestProjectData;
 import org.eclipse.syson.services.diagrams.api.IGivenDiagramSubscription;
 import org.junit.jupiter.api.DisplayName;
@@ -53,8 +53,7 @@ public class GVRelationshipTests extends AbstractIntegrationTests {
     @Autowired
     private IGivenDiagramSubscription givenDiagramSubscription;
 
-    @Sql(scripts = { GeneraViewRelationshipTestProjectData.SCRIPT_PATH }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
-            config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
+    @GivenSysONServer({ GeneraViewRelationshipTestProjectData.SCRIPT_PATH })
     @Sql(scripts = { "/scripts/cleanup.sql" }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
     @Test
     @DisplayName("GIVEN a General View with multiplicity range, WHEN the diagram is render, THEN the edge center label as an empty text")
@@ -65,16 +64,13 @@ public class GVRelationshipTests extends AbstractIntegrationTests {
                 GeneraViewRelationshipTestProjectData.GraphicalIds.DIAGRAM_ID);
         var flux = this.givenDiagramSubscription.subscribe(diagramEventInput);
 
-        Consumer<DiagramRefreshedEventPayload> initialDiagramContentConsumer = payload -> Optional.of(payload)
-                .map(DiagramRefreshedEventPayload::diagram)
-                .ifPresentOrElse(diagram -> {
-                    assertThat(diagram.getNodes()).hasSize(4);
-                    assertThat(diagram.getEdges()).hasSize(2);
-                    assertThat(diagram.getEdges().get(0).getCenterLabel()).isNotNull();
-                    assertThat(diagram.getEdges().get(0).getCenterLabel().text()).isBlank();
-                    assertThat(diagram.getNodes()).satisfiesOnlyOnce(node -> assertThat(node.getInsideLabel().getText()).contains("[2]"));
-
-                }, () -> fail("Missing diagram"));
+        Consumer<Object> initialDiagramContentConsumer = assertRefreshedDiagramThat(diagram -> {
+            assertThat(diagram.getNodes()).hasSize(4);
+            assertThat(diagram.getEdges()).hasSize(2);
+            assertThat(diagram.getEdges().get(0).getCenterLabel()).isNotNull();
+            assertThat(diagram.getEdges().get(0).getCenterLabel().text()).isBlank();
+            assertThat(diagram.getNodes()).satisfiesOnlyOnce(node -> assertThat(node.getInsideLabel().getText()).contains("[2]"));
+        });
 
         StepVerifier.create(flux)
                 .consumeNextWith(initialDiagramContentConsumer)
@@ -82,9 +78,7 @@ public class GVRelationshipTests extends AbstractIntegrationTests {
                 .verify(Duration.ofSeconds(10));
     }
 
-    @Sql(scripts = { GeneraViewRelationshipTestProjectData.SCRIPT_PATH }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
-            config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
-    @Sql(scripts = { "/scripts/cleanup.sql" }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
+    @GivenSysONServer({ GeneraViewRelationshipTestProjectData.SCRIPT_PATH })
     @Test
     @DisplayName("GIVEN a General View with an OccurrenceDefinition owning a OccurrenceUsage, WHEN the diagram is rendered, THEN an edge should be diplayed between the usage and the definition")
     public void checkOccurenceDefinitionNestedOccurrencesEdge() {
@@ -94,16 +88,14 @@ public class GVRelationshipTests extends AbstractIntegrationTests {
                 GeneraViewRelationshipTestProjectData.GraphicalIds.DIAGRAM_ID);
         var flux = this.givenDiagramSubscription.subscribe(diagramEventInput);
 
-        Consumer<DiagramRefreshedEventPayload> initialDiagramContentConsumer = payload -> Optional.of(payload)
-                .map(DiagramRefreshedEventPayload::diagram)
-                .ifPresentOrElse(diagram -> {
-                    Optional<Edge> nestedOcccurenceEdge = diagram.getEdges().stream().filter(
-                            edge -> GeneraViewRelationshipTestProjectData.GraphicalIds.OCC_1_ID.equals(edge.getTargetId()) && GeneraViewRelationshipTestProjectData.GraphicalIds.OCC_DEF_1_ID.equals(edge.getSourceId()))
-                            .findFirst();
-                    assertThat(nestedOcccurenceEdge.isPresent()).isTrue();
-                    assertThat(nestedOcccurenceEdge.get().getStyle().getSourceArrow() == ArrowStyle.FillDiamond);
+        Consumer<Object> initialDiagramContentConsumer = assertRefreshedDiagramThat(diagram -> {
+            Optional<Edge> nestedOcccurenceEdge = diagram.getEdges().stream().filter(
+                    edge -> GeneraViewRelationshipTestProjectData.GraphicalIds.OCC_1_ID.equals(edge.getTargetId()) && GeneraViewRelationshipTestProjectData.GraphicalIds.OCC_DEF_1_ID.equals(edge.getSourceId()))
+                    .findFirst();
+            assertThat(nestedOcccurenceEdge.isPresent()).isTrue();
+            assertThat(nestedOcccurenceEdge.get().getStyle().getSourceArrow() == ArrowStyle.FillDiamond);
 
-                }, () -> fail("Missing edge"));
+        });
 
         StepVerifier.create(flux)
                 .consumeNextWith(initialDiagramContentConsumer)
