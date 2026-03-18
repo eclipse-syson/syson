e34f4a36551cab5d673468df9d49100864b04ce6
Axel RICHARD <axel.richard@obeo.fr>
2026-03-18 11:10:23 +0100

    [2059] Fix Add existing elements tool
    Bug: https://github.com/eclipse-syson/syson/issues/2059
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

---
+++ backend/application/syson-application/src/test/java/org/eclipse/syson/application/data/GeneralViewAddExistingElementsActionFlowCompartmentTestProjectData.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.syson.application.data;
+
+/**
+ * Project data for the "GeneralView-AddExistingElements-ActionFlow-Compartment" project.
+ *
+ * @author arichard
+ */
+public class GeneralViewAddExistingElementsActionFlowCompartmentTestProjectData {
+
+    public static final String SCRIPT_PATH = "/scripts/database-content/GeneralView-AddExistingElements-ActionFlow-Compartment.sql";
+
+    public static final String EDITING_CONTEXT_ID = "cbb4b3b4-af8e-4a05-a419-19dbfd1cc94d";
+
+    /**
+     * Ids of graphical elements.
+     */
+    public static class GraphicalIds {
+
+        public static final String DIAGRAM_ID = "a0ca6720-66e0-40b7-a761-807474c0d773";
+
+        public static final String ACTION_1_NODE_ID = "62fff9fd-9f02-39e3-8446-1d638b9b053c";
+
+        public static final String ACTION_1_ACTIONFLOW_COMPARTMENT_NODE_ID = "f394cff3-b994-3b34-97c7-7d1a6807093e";
+    }
+
+    /**
+     * Ids for the semantic elements.
+     */
+    public static final class SemanticIds {
+
+        public static final String PACKAGE_1_ID = "8d4123ac-3ac5-412d-90f2-49282b923003";
+
+        public static final String GENERAL_VIEW_VIEW_USAGE_ID = "b67872fa-5900-48d3-88f0-e6ced193c8ec";
+
+        public static final String ACTION_1_ID = "40d86f92-e400-413d-8ec6-09fcf179b396";
+
+        public static final String ACTION_2_ID = "f0a37795-69e1-4ffe-becc-e55c20c70ac3";
+    }
+
+}
