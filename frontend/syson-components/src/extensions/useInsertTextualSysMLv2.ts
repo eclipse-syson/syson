b83d682b2f88b5f3001e4ffaa0f40edad0752084
Pierre-Charles David <pierre-charles.david@obeo.fr>
2026-04-07 10:51:02 +0200

    [cleanup] Fix typo in file name
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- frontend/syson-components/src/extensions/useInsertTextualSysMLv2.ts
+++ frontend/syson-components/src/extensions/useInsertTextualSysMLv2.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 Obeo.
+ * Copyright (c) 2025, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -19,7 +19,7 @@ import {
   GQLInsertTextualSysMLv2MutationData,
   GQLInsertTextualSysMLv2Payload,
   UseInsertTextualSysMLv2Value,
-} from './useInsertTextualSysML.v2types';
+} from './useInsertTextualSysMLv2.types';
 
 const insertTextualSysMLv2Mutation = gql`
   mutation insertTextualSysMLv2($input: InsertTextualSysMLv2Input!) {
