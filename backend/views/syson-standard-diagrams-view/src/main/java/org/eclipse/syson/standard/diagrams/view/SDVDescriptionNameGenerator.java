e2bf28ea21e350fe318ad4b33b76652ab48bf0b1
Guillaume Coutable <guillaume.coutable@obeo.fr>
2026-05-21 09:31:47 +0200

    [2231] Add simple concern creation tool from Requirement
    Bug: https://github.com/eclipse-syson/syson/issues/2231
    Signed-off-by: Guillaume Coutable <guillaume.coutable@obeo.fr>

--- backend/views/syson-standard-diagrams-view/src/main/java/org/eclipse/syson/standard/diagrams/view/SDVDescriptionNameGenerator.java
+++ backend/views/syson-standard-diagrams-view/src/main/java/org/eclipse/syson/standard/diagrams/view/SDVDescriptionNameGenerator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024, 2025 Obeo.
+ * Copyright (c) 2024, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -42,6 +42,9 @@ public class SDVDescriptionNameGenerator extends DescriptionNameGenerator {
         } else if (SysmlPackage.eINSTANCE.getRequirementUsage_RequiredConstraint().equals(eReference)
                 || SysmlPackage.eINSTANCE.getRequirementDefinition_RequiredConstraint().equals(eReference)) {
             name = "New Require constraint";
+        } else if (SysmlPackage.eINSTANCE.getRequirementUsage_FramedConcern().equals(eReference)
+                || SysmlPackage.eINSTANCE.getRequirementDefinition_FramedConcern().equals(eReference)) {
+            name = "New framed concern";
         }
         return name;
     }
