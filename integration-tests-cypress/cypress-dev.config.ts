f86336ded06afdcb84c22f60a8b59b1480233886
Guillaume Coutable <guillaume.coutable@obeo.fr>
2026-06-26 15:55:38 +0200

    [test] Set the same cypress retries configuration as Playwright
    Signed-off-by: Guillaume Coutable <guillaume.coutable@obeo.fr>

--- integration-tests-cypress/cypress-dev.config.ts
+++ integration-tests-cypress/cypress-dev.config.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024, 2025 Obeo.
+ * Copyright (c) 2024, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -20,6 +20,7 @@ export default defineConfig({
     mochaFile: 'target/result-[hash].xml',
     toConsole: true,
   },
+  retries: 0,
   viewportWidth: 1920,
   viewportHeight: 1080,
   env: {
--- integration-tests-cypress/cypress/cypress.d.ts
+++ integration-tests-cypress/cypress/cypress.d.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024, 2025 Obeo.
+ * Copyright (c) 2024, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -15,6 +15,7 @@ import { CreateDocumentData } from './support/server/createDocumentCommand.types
 import { CreateProjectData } from './support/server/createProjectCommand.types';
 import { DeleteProjectData } from './support/server/deleteProjectCommand.types';
 import { GetCurrentEditingContextIdData } from './support/server/getCurrentEditingContextId.types';
+import { GetLibraryIdData } from './support/server/getLibraryId.types';
 import { MutationResponse, QueryResponse } from './support/server/graphql.types';
 
 export {};
